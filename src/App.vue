a241218538543e8f735c6e0e32f6767ddb2e69f2
ankub <ankub@smile.fr>
2020-01-17 15:28:44 +0200

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <b-container>
+      <b-row>
+        <b-col sm="6" offset="3">
+          <QuestionBox
+            :currentQuestion="questions[index]"
+          />
+        </b-col>
+      </b-row>
+    </b-container>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/header.vue'
+import QuestionBox from './components/questionBox.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header,
+    QuestionBox
+  },
+  data(){
+    return{
+      questions: [],
+      index: 0
+    }
+  },
+  methods: {
+    next (){
+      
+    }
+  },
+  mounted: function() {
+    fetch('https://opentdb.com/api.php?amount=10&category=25', {
+      method: 'get'
+    })
+    .then((response) => {
+      return response.json()
+    })
+    .then((jsonData) =>{
+      this.questions = jsonData.results
+    })
   }
 }
 </script>
